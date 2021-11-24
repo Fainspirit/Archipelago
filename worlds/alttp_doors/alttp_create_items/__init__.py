6a4ad3bdbd154747f0399e8b6ddee6ce144e88f6
Alexander Chambers <fainspirit@gmail.com>
2021-11-24 00:22:17 -0600

    Added triforce and events fill

--- worlds/alttp_doors/alttp_create_items/__init__.py
+++ worlds/alttp_doors/alttp_create_items/__init__.py
@@ -3,6 +3,7 @@ from .beemizer import handle_beemizer
 from .capacity_upgrades import handle_capacity_upgrades
 from .custom import handle_custom_pool
 from .difficulty import handle_difficulty
+from .events import handle_events
 from .item_groups import handle_item_groups
 from .junk import handle_junk
 from .key_drop_shuffle import handle_key_drop_shuffle
@@ -11,5 +12,6 @@ from .retro import handle_retro_mode
 from .shops import handle_shops
 from .swords import handle_swords
 from .timed_clocks import handle_timed_clocks
+from .triforce import handle_triforce_placement
 from .triforce_hunt import handle_triforce_hunt
 from .vanilla import create_vanilla_pool
