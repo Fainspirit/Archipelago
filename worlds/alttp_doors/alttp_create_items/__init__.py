b73fef85fff1b8a1e5d80d0dd74ca4e463e5e1cf
Alexander Chambers <fainspirit@gmail.com>
2021-11-24 02:46:59 -0600

    move events and triforce to pre fill, add crystal placement

--- worlds/alttp_doors/alttp_create_items/__init__.py
+++ worlds/alttp_doors/alttp_create_items/__init__.py
@@ -3,7 +3,6 @@ from .beemizer import handle_beemizer
 from .capacity_upgrades import handle_capacity_upgrades
 from .custom import handle_custom_pool
 from .difficulty import handle_difficulty
-from .events import handle_events
 from .item_groups import handle_item_groups
 from .junk import handle_junk
 from .key_drop_shuffle import handle_key_drop_shuffle
@@ -12,6 +11,5 @@ from .retro import handle_retro_mode
 from .shops import handle_shops
 from .swords import handle_swords
 from .timed_clocks import handle_timed_clocks
-from .triforce import handle_triforce_placement
 from .triforce_hunt import handle_triforce_hunt
 from .vanilla import create_vanilla_pool
--- worlds/alttp_doors/alttp_pre_fill/__init__.py
+++ worlds/alttp_doors/alttp_pre_fill/__init__.py
@@ -1,1 +1,4 @@
 """pre_fill methods"""
+from .crystals import handle_crystals
+from .events import handle_events
+from .triforce import handle_triforce_placement
